5389d98197b615a2baec1d9051b230aac08685f8
FlynnKing <davidedg97@gmail.com>
2022-02-21 14:38:22 +0100

    carte e tavolo

---
+++ tavolo.py
@@ -0,0 +1,27 @@
+import zmq
+class Tavolo: 
+    #n_tavoli = [] #se volgiamo complicare il gioco
+    giocatori = []
+    info_giocatore_singolo = ["indirizzo": "", "nome" : ""]
+    max_giocatori = 0
+    def __init__(self):
+        self.n_giocatori = num_players
+    def richiesta_giocatori(self):
+        #RICEVI RICHIESTA per il max_giocatori con un for
+        #chiediamo la stringa bit (lista), la convertiamo in str e prendiamo:
+        #indirizzo client, nome (input_utente), 
+        pass
+    
+    def richiesta_giocatori():
+        #parte client server
+        
+        #return lista giocatori
+        pass
+    def 
+        
+        
+    
+class Mazziere:
+        
+    
+    
