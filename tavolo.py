4a8b7626a623440a05fc144ec32e6fed134a0076
FlynnKing <davidedg97@gmail.com>
2022-02-21 15:53:20 +0100

    metodi client server

--- tavolo.py
+++ tavolo.py
@@ -2,20 +2,29 @@ import zmq
 class Tavolo: 
     #n_tavoli = [] #se volgiamo complicare il gioco
     giocatori = []
-    info_giocatore_singolo = ["indirizzo": "", "nome" : ""]
+    info_giocatore_singolo = ["richiesta":"","indirizzo": "", "nome" : "", ]
     max_giocatori = 0
     def __init__(self):
-        self.n_giocatori = num_players
-    def richiesta_giocatori(self):
-        #RICEVI RICHIESTA per il max_giocatori con un for
-        #chiediamo la stringa bit (lista), la convertiamo in str e prendiamo:
-        #indirizzo client, nome (input_utente), 
         pass
-    
-    def richiesta_giocatori():
-        #parte client server
+    def lista_giocatori(self):
+        #RICEVI UTENTE o (ciclando fino al massimo di utenti possibili (3))
         
-        #return lista giocatori
+        
+    def scelta_metodo(self):
+        #chiediamo la stringa bit (lista), la convertiamo in str e prendiamo
+        #il parametro "richiesta"
+        #
+        pass
+    #in base alla richiesta (nel dizionario) di ogni giocatore, si procede con 
+    #il metodo adeguato: validazione_player(bool), pescata(str), 
+    def richiesta_giocatori():
+        #MANDA RICHIESTA: per avviare un timer. Poi chiudi.
+        #RICEVI RICHIESTA: per il max_giocatori con un for (ache il timer può 
+        #mandare la richiesta per dire che l'utente non si è unito, vai avanti).
+            #appeni info_giocatore_singolo ("indirizzo" e "nome") a giocatori
+            #MANDA RICHIESTA: per un timer solo se non si arriva a max_giocatori
+            #else  MANDA RICHIESTA abbaimo tutti i giocatori, il gioco può partire
+        #return self.giocatori
         pass
     def 
         
